43c5b38fad50caab0a1cc44fd8125d619f1f77f0
Alejandro Di Battista <alejandrodibattista@gmail.com>
2021-04-23 21:38:13 -0300

    Update Agenda.cs

--- Agenda.cs
+++ Agenda.cs
@@ -1,29 +1,29 @@
-﻿using System;
+﻿using System.IO;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using static System.Console;
 
-namespace Demo01
-{
-    class Program
-    {
+namespace Demo01 {
+
+    class Program {
+
         public class Contacto {
-            public string Nombre;
+            public string Nombre {get; set; }
             public string Apellido;
             public string Telefono;
-            public int Edad; 
-
+            public int Edad;
+    
             public string NombreCompleto => $"{Apellido}, {Nombre}" ;
         }
 
-        public class Agenda{
+        public class Agenda {
             private List<Contacto> contactos;
 
-            public Agenda(){
+            public Agenda() {
                 contactos = new List<Contacto>();
             }
 
-            public void Leer(){
+            public void Leer() {
                 var lineas = File.ReadAllLines(@"agenda.txt");
                 contactos = new List<Contacto>();
                 foreach(var linea in lineas) { 
@@ -31,136 +31,126 @@ namespace Demo01
                     var nombre   = datos[0];
                     var apellido = datos[1];
                     var telefono = datos[2];
-                    var edades   = int.Parse(datos[3]);
-                    var contacto = new Contacto{ Nombre = nombre, Apellido = apellido, Telefono = telefono, Edad = edades };
+                    var edad     = int.Parse(datos[3]);
+                    var contacto = new Contacto{ Nombre = nombre, Apellido = apellido, Telefono = telefono, Edad = edad };
                     Agregar( contacto  );
                 }
             }
 
-            public void Escribir(){
-                for(var i = 0 ; i < contactos.Count; i++){
-                    var item = contactos[i];
-
-                }
-
-                foreach(var item in contactos){
-
-                }
-                contactos.ForEach( item => "");
-
+            public void Escribir() {
                 var lineas = contactos.Select( c => $"{c.Nombre},{c.Apellido},{c.Telefono},{c.Edad}");
-                Console.WriteLine("Escribiendo Agenda... {0}", lineas.Count() );
+                WriteLine($"Escribiendo Agenda... {lineas.Count()}" );
                 File.WriteAllLines(@"agenda.txt", lineas);
             }
             
-            public void Listar(){
+            public void Listar() {
                 var i = 1;
-                foreach(var contacto in contactos){
-                    Console.WriteLine($" {i}. {contacto.NombreCompleto,-30} | {contacto.Telefono} | {contacto.Edad:##0} años");
-                    i++;
+                foreach(var contacto in contactos) {
+                    WriteLine($" {i++}. {contacto.NombreCompleto,-40} | {contacto.Telefono} | {contacto.Edad:##0} años");
                 }
             }
 
-            public void Agregar(Contacto contacto){
+            public void Agregar(Contacto contacto) {
                 if(contacto == null) return;
                 contactos.Add(contacto);
             }
 
-            public void Editar(Contacto contacto, int posicion){
+            public void Editar(Contacto contacto, int posicion) {
                 if(contacto == null) return;
                 contactos[posicion - 1] = contacto;
             }
             
-            public void Borrar(int posicion){
-                contactos.Remove( contactos[posicion - 1] );
-            }
-
-            public int Cantidad(){
-                return contactos.Count;
+            public void Borrar(int posicion) {
+                contactos.RemoveAt( posicion - 1 );
             }
         }
 
-        static Contacto IngresarContacto(){
+        static Contacto IngresarContacto() {
             var  actual = new Contacto();
-            Console.Write("   Nombre   :> "); actual.Nombre   = Console.ReadLine();
-            Console.Write("   Apellido :> "); actual.Apellido = Console.ReadLine();
-            Console.Write("   Teléfono :> "); actual.Telefono = Console.ReadLine();
-            Console.Write("   Edad     :> "); actual.Edad     = int.Parse(Console.ReadLine());
-            if(actual.Nombre.Length < 2){
-                Console.Write("Debe ingresar un Nombre valido");
-                Console.ReadLine();
+            Write("   Nombre   :> "); actual.Nombre   = ReadLine();
+            Write("   Apellido :> "); actual.Apellido = ReadLine();
+            Write("   Teléfono :> "); actual.Telefono = ReadLine();
+            Write("   Edad     :> "); actual.Edad     = int.Parse(ReadLine());
+            
+            if(actual.Nombre.Length < 2) {
+                Write("Debe ingresar un Nombre valido");
+                ReadLine();
                 return null;
             }
-            if(actual.Apellido.Length < 2){
-                Console.Write("Debe ingresar un Apellido valido");
-                Console.ReadLine();
+            
+            if(actual.Apellido.Length < 2) {
+                Write("Debe ingresar un Apellido valido");
+                ReadLine();
                 return null;
             }
-            if(actual.Telefono.Length < 7){
-                Console.Write("Debe ingresar un Telefono valido");
-                Console.ReadLine();
+            
+            if(actual.Telefono.Length < 7) {
+                Write("Debe ingresar un Telefono valido");
+                ReadLine();
                 return null;
             }
-            if(actual.Edad < 0 || actual.Edad > 99){
-                Console.Write("Debe ingresar una Edad valida");
-                Console.ReadLine();
+
+            if(actual.Edad < 0 || actual.Edad > 99) {
+                Write("Debe ingresar una Edad valida");
+                ReadLine();
                 return null;
             }
+            
             return actual;
         }
 
-        static int ElegirContacto(){
-            Console.WriteLine(" > Elegir contacto");
+        static int ElegirContacto() {
+            WriteLine(" > Elegir contacto");
             agenda.Listar();
-            Console.WriteLine();
-            Console.WriteLine(" 0. Cancelar");
-            Console.Write(" :> ");
-            var opcion = int.Parse(Console.ReadLine());
+            WriteLine();
+            WriteLine(" 0. Cancelar");
+            Write(" :> ");
+            var opcion = int.Parse(ReadLine());
             return opcion;
         }
 
-        static void AgregarContacto(){
-            Console.WriteLine(" Agregando Contacto...");
+        static void AgregarContacto() {
+            WriteLine(" Agregando Contacto...");
             agenda.Agregar( IngresarContacto() );
         }
 
-        static void EditarContacto(){
-            Console.WriteLine(" Editando Contacto...");
+        static void EditarContacto() {
+            WriteLine(" Editando Contacto...");
             var i = ElegirContacto();
             if(i == 0) return;
             agenda.Editar( IngresarContacto(), i);
         }
 
-        static void BorrarContacto(){
-            Console.WriteLine(" Borrando Contacto...");
+        static void BorrarContacto() {
+            WriteLine(" Borrando Contacto...");
             var i = ElegirContacto();
             if(i == 0) return;
             agenda.Borrar(i);
         }
 
-        static void ListarContactos(){
-            Console.WriteLine(" Listado de Contactos...");
+        static void ListarContactos() {
+            WriteLine(" Listado de Contactos...");
             agenda.Listar();
-            Console.Write(" :> "); Console.ReadLine();
-            Console.WriteLine();
+            Write(" :> "); ReadLine();
+            WriteLine();
         }
 
         static Agenda agenda;
 
-        static void Menu(){
+        static void Menu() {
             while(true) {
-                Console.WriteLine();
-                Console.WriteLine(" > Mini Agenda");
-                Console.WriteLine(" 1. Agregar Contacto");
-                Console.WriteLine(" 2. Editar Contacto");
-                Console.WriteLine(" 3. Borrar Contacto");
-                Console.WriteLine(" 4. Listar Contactos");
-                Console.WriteLine();
-                Console.WriteLine(" 0. Terminar");
-                Console.Write(" :> ");
-                var opcion = Console.ReadLine();
-                Console.WriteLine();
-                switch(opcion){
+                WriteLine();
+                WriteLine(" > Mini Agenda");
+                WriteLine(" 1. Agregar Contacto");
+                WriteLine(" 2. Editar Contacto");
+                WriteLine(" 3. Borrar Contacto");
+                WriteLine(" 4. Listar Contactos");
+                WriteLine();
+                WriteLine(" 0. Terminar");
+                Write(" :> ");
+                var opcion = ReadLine();
+                WriteLine();
+                switch(opcion) {
                     case "1": 
                         AgregarContacto();
                         break;
@@ -177,10 +167,10 @@ namespace Demo01
                     case "" :
                         return;
                 }
-            } 
+            }
         }
 
-        static void Main(string[] args){
+        static void Main(string[] args) {
             agenda = new Agenda();
             agenda.Leer();
             Menu();
